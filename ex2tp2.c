07e7cd801b1f008192713ec8c28ad925a6aea758
nourhenebelhiba <32389379+nourhenebelhiba@users.noreply.github.com>
2017-10-04 12:40:04 +0100

    Add files via upload

---
+++ ex2tp2.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : ex2tp2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <math.h>
+int main() {
+float x;
+printf ("donner un reel");
+scanf ("%f",&x);
+printf ("%f",fabs(x));
+return 0;
+}
